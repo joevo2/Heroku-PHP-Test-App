ab44db24c83ea3ad331a0973fe397112af515646
Joel Yek <joevo2@gmail.com>
2014-03-16 18:54:47 +0800

    added postgresql database connection

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 	echo "Hello World";
-	phpinfo();
-?>
+	
+	#PostgreSQL Databse Connection
+	$db = pg_connect("dbname=dectdccb9d6neh host=ec2-107-20-191-205.compute-1.amazonaws.com port=5432 user=ilqmgopvoboqqe password=pSt_F9c9Yc4yuJMrYdigezqXAr sslmode=require");
+	if (!db) {
+		echo "Database connection error";
+		exit;
+	}
+?>	
