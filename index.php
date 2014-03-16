10811569d50b655d3fc330b3274a05859b75dc75
Joel Yek <joevo2@gmail.com>
2014-03-16 19:10:25 +0800

    modefied error reporting on db connection

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
 	echo "Hello World";
 	
 	#PostgreSQL Databse Connection
-	$db = pg_connect("dbname=dectdccb9d6neh host=ec2-107-20-191-205.compute-1.amazonaws.com port=5432 user=ilqmgopvoboqqe password=pSt_F9c9Yc4yuJMrYdigezqXAr sslmode=require");
-	if (!db) {
-		echo "Database connection error";
-		exit;
-	}
-
+	$db = pg_connect("dbname=dectdccb9d6neh host=ec2-107-20-191-205.compute-1.amazonaws.com 
+		port=5432 user=ilqmgopvoboqqe password=pSt_F9c9Yc4yuJMrYdigezqXAr 
+		sslmode=require") or die('Could not connect: ' . pg_last_error());;
+	
 	#Query, similar to MySQL
 	#$result = pg_query($db,"SELECT ...")
 ?>	
