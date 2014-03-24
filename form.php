243c4964c8b3a93f662483b98c1c6a2da831cb56
Joel Yek <joevo2@gmail.com>
2014-03-24 12:27:27 +0800

    added localhost connection

---
+++ form.php
@@ -0,0 +1,7 @@
+	<form action="" method="post">
+		<input type="text" name="name" placeholder="Your Name">
+		<br>
+		<input type="text" name="address" placeholder="Your Address">
+		<br>
+		<input type="submit">
+	</form>
--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 	<head>
 		<title>HELP CAT Carpool</title>
 		<style type="text/css">
-			iframe {
-				max-width: 600px;
+			html {
+				width: 80%;
+				margin: 0 auto;
+			}
+
+			.mapbox {
+				max-width: 450px;
 				width: 100%;
 			}
 		</style>
@@ -12,43 +17,22 @@
 	<body>
 		<h1>HELP CAT Car Pool Database</h1>
 		<!-- Google Map Embed -->
-		<iframe
-		  width="600"
-		  height="450"
+		<iframe class="mapbox" 
+		  width="500"
+		  height="500"
 		  frameborder="0" style="border:0"
 		  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDsA-rWJVhqWo5XhCH3f6eOwjQol2hyCCc
 		    &q=HELP+College+of+Arts+and+Technology">
 		</iframe>
-
-		<form action="" method="post">
-			<input type="text" name="name" placeholder="Your Name">
-			<br>
-			<input type="text" name="address" placeholder="Your Address">
-			<br>
-			<input type="submit">
-		</form>
-
+		<?php include "form.php" ?>
 	</body>
 <html>
 
 <?php
 	#PHP Function and connection
 	include 'function.php';
-	
-	#Create Table
-	$sql = "CREATE TABLE test (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(60) NOT NULL,
-		address VARCHAR(80) NOT NULL)";
-	$query = pg_query($sql);
-	if ($query)
-  		echo "Table Created<br>"; 
-	else { 
-  		echo "An error Occured! ".pg_last_error()."<br>";
-	}
-	
 
-
-	//queryInsert($con, "test", array('name', 'address'),array("$_POST[name]", "$_POST[address]"));
-	queryDisplay(queryAll($con, "test"));
+	#Form insertion
+	queryInsert($con, "test", array('name', 'address'),array("$_POST[name]", "$_POST[address]"));
+	//queryDisplay(queryAll($con, "test"));
 ?>	
