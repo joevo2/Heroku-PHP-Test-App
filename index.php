20cee8555e06019d092e530a523584be692ca010
Joel Yek <joevo2@gmail.com>
2014-04-28 15:32:33 +0800

    added html location tracking and map

--- index.php
+++ index.php
@@ -3,9 +3,64 @@
 	<head>
 		<title>Carpool for HELP CAT</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<style type="text/css">
-			
-		</style>
+		<style>
+	      #map-canvas {
+	        height: 500px;
+	        max-width: 500px;
+	        margin: 0px;
+	        padding: 0px
+	      }
+    	</style>
+	    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+	    <script>
+		      function initialize() {
+				  var mapOptions = {
+				    zoom: 16
+				  };
+				  map = new google.maps.Map(document.getElementById('map-canvas'),
+				      mapOptions);
+
+				  // Try HTML5 geolocation
+				  if(navigator.geolocation) {
+				    navigator.geolocation.getCurrentPosition(function(position) {
+				      var pos = new google.maps.LatLng(position.coords.latitude,
+				                                       position.coords.longitude);
+
+				      var infowindow = new google.maps.InfoWindow({
+				        map: map,
+				        position: pos,
+				        content: 'You!'
+				      });
+
+				      map.setCenter(pos);
+				    }, function() {
+				      handleNoGeolocation(true);
+				    });
+				  } else {
+				    // Browser doesn't support Geolocation
+				    handleNoGeolocation(false);
+				  }
+				}
+
+				function handleNoGeolocation(errorFlag) {
+				  if (errorFlag) {
+				    var content = 'Error: The Geolocation service failed.';
+				  } else {
+				    var content = 'Error: Your browser doesn\'t support geolocation.';
+				  }
+
+				  var options = {
+				    map: map,
+				    position: new google.maps.LatLng(60, 105),
+				    content: content
+				  };
+
+				  var infowindow = new google.maps.InfoWindow(options);
+				  map.setCenter(options.position);
+				}
+
+				google.maps.event.addDomListener(window, 'load', initialize);
+	    </script>
 	</head>
 	<body>
 		<!--?php include "views/fblogin.php"; ?-->
@@ -21,7 +76,7 @@
 		<h1>Carpool for HELP CAT</h1>
 		<!-- Google Map Embed -->
 		<div class="col-1">
-			<img src="http://placehold.it/600x700&text=Map">
+			<div id="map-canvas"></div>
 		</div>
 		<div class="col-1">
 			<div>
--- map.php
+++ map.php
@@ -1,33 +1,39 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <style type="text/css">
-      html { height: 100% }
-      body { height: 100%; margin: 0; padding: 0 }
-      #map-canvas { height: 100% }
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Simple markers</title>
+    <style>
+      #map-canvas {
+        height: 500px;
+        width: 500px;
+        margin: 0px;
+        padding: 0px
+      }
     </style>
-    <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?sensor=false">
-    </script>
-  </head>
-  <body>
-    <div id="map-canvas">
-      <script type="text/javascript">
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <script>
+      function initialize() {
         var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
         var mapOptions = {
           zoom: 4,
           center: myLatlng
         }
-        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-        // To add the marker to the map, use the 'map' property
         var marker = new google.maps.Marker({
             position: myLatlng,
             map: map,
-            title:"Hello World!"
+            title: 'Hello World!'
         });
+      }
+
+      google.maps.event.addDomListener(window, 'load', initialize);
     </script>
-    </div>
+  </head>
+  <body>
+    <div id="map-canvas"></div>
   </body>
 </html>
+
