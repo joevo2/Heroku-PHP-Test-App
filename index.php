0f36ee475cf6a8ec807e75b3686b915ff2a17479
Joel Yek <joevo2@gmail.com>
2014-05-03 12:48:56 +0800

    Separated map

--- index.php
+++ index.php
@@ -3,64 +3,7 @@
 	<head>
 		<title>Carpool for HELP CAT</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<style>
-	      #map-canvas {
-	        height: 500px;
-	        max-width: 500px;
-	        margin: 0px;
-	        padding: 0px
-	      }
-    	</style>
-	    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-	    <script>
-		      function initialize() {
-				  var mapOptions = {
-				    zoom: 16
-				  };
-				  map = new google.maps.Map(document.getElementById('map-canvas'),
-				      mapOptions);
-
-				  // Try HTML5 geolocation
-				  if(navigator.geolocation) {
-				    navigator.geolocation.getCurrentPosition(function(position) {
-				      var pos = new google.maps.LatLng(position.coords.latitude,
-				                                       position.coords.longitude);
-
-				      var infowindow = new google.maps.InfoWindow({
-				        map: map,
-				        position: pos,
-				        content: 'You!'
-				      });
-
-				      map.setCenter(pos);
-				    }, function() {
-				      handleNoGeolocation(true);
-				    });
-				  } else {
-				    // Browser doesn't support Geolocation
-				    handleNoGeolocation(false);
-				  }
-				}
-
-				function handleNoGeolocation(errorFlag) {
-				  if (errorFlag) {
-				    var content = 'Error: The Geolocation service failed.';
-				  } else {
-				    var content = 'Error: Your browser doesn\'t support geolocation.';
-				  }
-
-				  var options = {
-				    map: map,
-				    position: new google.maps.LatLng(60, 105),
-				    content: content
-				  };
-
-				  var infowindow = new google.maps.InfoWindow(options);
-				  map.setCenter(options.position);
-				}
-
-				google.maps.event.addDomListener(window, 'load', initialize);
-	    </script>
+		<!--?php include "views/map.php" ?-->
 	</head>
 	<body>
 		<!--?php include "views/fblogin.php"; ?-->
@@ -77,6 +20,8 @@
 		<!-- Google Map Embed -->
 		<div class="col-1">
 			<div id="map-canvas"></div>
+			<!-- Image placeholder -->
+			<img src="http://placehold.it/500x500">
 		</div>
 		<div class="col-1">
 			<div>
@@ -87,6 +32,7 @@
 					queryDisplay(queryAll($con, $table));
 				?>	
 			</div>	
+			<h3>Driver Nearby</h3>
 		</div>
 	</body>
 <html>
--- views/form.php
+++ views/form.php
@@ -5,13 +5,15 @@
 		$errName = $errEmail = $errSex = NULL;
 	?>
 	<form action="" method="post">
-		<input class="form" type="text" name="name" placeholder="Your Name">
+		<input class="form" type="text" name="name" placeholder="Name">
 		<span class="err">* <?php echo $errName;?> </span><br>
-		<input class="form" type="text" name="email" placeholder="Your email">
+		<input class="form" type="text" name="email" placeholder="Email">
 		<span class="err">* <?php echo $errEmail;?> </span><br>
 		<input type="radio" name="sex" value="Male">Male<br>
 		<input type="radio" name="sex" value="Female">Female<br>
 		<span class="err">* <?php echo $errSex;?> </span><br>
+		
+		<input type="checkbox">I agree to to <a href="view/terms.html">CarPool</a> Terms <br>
 		<input class="btn" type="submit" value="Submit"><br>
 	</form>
 
