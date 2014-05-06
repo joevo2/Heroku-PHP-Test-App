7ba6b17fb00187a26be5ee9cbf755e526a0b0f5d
Joel Yek <joevo2@gmail.com>
2014-05-06 10:43:33 +0800

    modified sign up/in css

--- views/admin.php
+++ views/admin.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>CP Admin Panel</title>
 		<link rel="stylesheet" type="text/css" href="../style.css">
+		<meta name="viewport" content="width=device-width, user-scalable=no">
 	</head>
 	<body>
 		<a href="../index.php"><h1>Carpool for HELP CAT</h1></a>
